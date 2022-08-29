8763215c3414b93bd5ab138c90777302326ee95d
Emigdio Espinosa <millosespinosa@gmail.com>
2022-08-29 11:46:52 -0500

    Hemos terminado la clase de hoy, nos falta terminar el reloj

--- P02/reloj.js
+++ P02/reloj.js
@@ -1,2 +1,36 @@
 var canvas = document.getElementById('canvas');
-var context = canvas.getContext('2d');
+var context = canvas.getContext('2d'),
+fontHeight = 15,
+margin = 35,
+handTruncation = canvas.width / 25,
+hourHandTruncation = canvas.width / 10,
+numeralSpacing = 20,
+radius = canvas.width / 4 - margin, 
+handRadius = radius + numeralSpacing; 
+
+//funcion drawCircle
+function drawCircle()
+{
+      context.beginPath();
+      context.arc(canvas.width / 2, canvas.height / 2, 0, Math.PI * 2, true);
+}
+//funcion drawCenter
+function drawCenter()
+{
+      context.beginPath();
+      context.arc(canvas.widht / 2, canvas.height / 2, 5, 0, Math.PI * 2, true);
+      context.fill();
+}
+//funcion drawClock
+function drawClock()
+{
+      context.clearRect(0,0, canvas.width, canvas.height);
+      drawCircle();
+      drawCenter(); 
+}
+
+//Inicializamos
+context.font = fontHeight + 'px Arial';
+loop = setInterval(drawClock, 1000);
+
+
