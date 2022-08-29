f7d1fc66a02e101567a719b5abf17fd6108293b4
Emigdio Espinosa <millosespinosa@gmail.com>
2022-08-29 11:14:28 -0500

    Agregamos un texto al canvas

---
+++ P02/canvas.js
@@ -0,0 +1,8 @@
+var canvas = document.getElementById('canvas');
+var context = canvas.getContext('2d');
+
+context.font = '30pt Arial';
+context.fillStyle = 'cornflowerblue';
+context.strokeStyle = 'blue';
+
+context.fillText('Hola CANVAS',canvas.width/2-120,canvas.height/2);
