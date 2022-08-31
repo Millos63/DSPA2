0f4d84d02359fd2325d466188610b7257001914d
Emigdio Espinosa <millosespinosa@gmail.com>
2022-08-31 15:41:01 -0500

    Terminamos la circunferencia y los numeros del reloj

--- P02/reloj.js
+++ P02/reloj.js
@@ -4,8 +4,10 @@ fontHeight = 15,
 margin = 35,
 handTruncation = canvas.width / 25,
 hourHandTruncation = canvas.width / 10,
-numeralSpacing = 20,
-radius = canvas.width / 4 -margin, 
+numeralSpacing = -10,
+radius = canvas.width / 3 - margin, 
+//Superficie: ancho x alto 300 x 150.
+//Circulo: radio = 75; ancho x alto 75 x 75
 handRadius = radius + numeralSpacing; //Espacio de las manesillas 
 
 //funcion drawCircle
@@ -28,10 +30,29 @@ function drawClock()
       context.clearRect(0, 0, canvas.width, canvas.height);
       drawCircle();
       drawCenter(); 
+      drawNumerals();
+}
+
+function drawNumerals()
+{
+      //Con coma al final podemos crear varias variables
+      var numerals = [1,2,3,4,5,6,7,8,9,10,11,12], //Arreglo
+      angle = 0,
+      numeralWidth = 0; 
+      
+      numerals.forEach(function(numeral) //Recorremos arreglo y se define la function
+      { 
+      angle = Math.PI / 6 * (numeral - 3);
+      numeralWidth = context.measureText
+      (numeral).width;
+      context.fillText(numeral, 
+            canvas.width/2 + Math.cos(angle)*(handRadius) - numeralWidth/2,
+            canvas.height/2+Math.sin(angle)*(handRadius) + fontHeight /3
+            );
+      });
 }
 
 //Inicializamos
 context.font = fontHeight + 'px Arial';
 loop = setInterval(drawClock, 1000); //El intervalo de tiempo
-
 
