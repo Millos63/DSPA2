dae9a738f92585ec1af5cb2a6822df5139951463
Emigdio Espinosa <millosespinosa@gmail.com>
2022-08-31 14:19:33 -0500

    Primer commit del dia, continuamos creando el reloj

--- P02/reloj.js
+++ P02/reloj.js
@@ -6,19 +6,20 @@ handTruncation = canvas.width / 25,
 hourHandTruncation = canvas.width / 10,
 numeralSpacing = 20,
 radius = canvas.width / 4 - margin, 
-handRadius = radius + numeralSpacing; 
+handRadius = radius + numeralSpacing; //Espacio de las manesillas 
 
 //funcion drawCircle
 function drawCircle()
 {
       context.beginPath();
       context.arc(canvas.width / 2, canvas.height / 2, 0, Math.PI * 2, true);
+      context.fill();
 }
 //funcion drawCenter
 function drawCenter()
 {
       context.beginPath();
-      context.arc(canvas.widht / 2, canvas.height / 2, 5, 0, Math.PI * 2, true);
+      context.arc(canvas.widht / 2, canvas.height / 2, radius, 0, Math.PI * 2, true);
       context.fill();
 }
 //funcion drawClock
@@ -31,6 +32,6 @@ function drawClock()
 
 //Inicializamos
 context.font = fontHeight + 'px Arial';
-loop = setInterval(drawClock, 1000);
+loop = setInterval(drawClock, 1000); //El intervalo de tiempo
 
 
