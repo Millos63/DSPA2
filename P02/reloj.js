dedd50c29df669dc43bbf489d4caed0f57ba9635
Emigdio Espinosa <millosespinosa@gmail.com>
2022-09-05 11:30:53 -0500

    Hemos terminado el reloj

--- P02/reloj.js
+++ P02/reloj.js
@@ -24,6 +24,8 @@ function drawCenter()
       context.arc(canvas.width / 2, canvas.height / 2, 3, 0, Math.PI * 2, true);
       context.fill(); //Para ponerle el puntito en el centro.
 }
+
+
 //funcion drawClock
 function drawClock()
 {
@@ -31,6 +33,7 @@ function drawClock()
       drawCircle();
       drawCenter(); 
       drawNumerals();
+      drawHands();
 }
 
 function drawNumerals()
@@ -47,12 +50,55 @@ function drawNumerals()
       (numeral).width;
       context.fillText(numeral, 
             canvas.width/2 + Math.cos(angle)*(handRadius) - numeralWidth/2,
-            canvas.height/2+Math.sin(angle)*(handRadius) + fontHeight /3
+            canvas.height/2+Math.sin(angle)*(handRadius) + fontHeight / 3
             );
       });
 }
 
+
+
+//Funcion drawHand Dibujara una de las manecillas
+function drawHand(value)
+{
+                  //360 grados
+      var angle = (Math.PI * 2) * (value/60) - Math.PI/2;  
+      var handRadius = radius - handTruncation;
+      context.beginPath();       // Start a new path
+      context.moveTo(canvas.width / 2, canvas.height / 2);    // Mueve al centro del reloj
+      context.lineTo(canvas.width/2 + Math.cos(angle)*(handRadius), canvas.height/2 + Math.sin(angle)*(handRadius) );  // Draw a line to (150, 100)
+      context.stroke();          // Render the path
+}
+
+//funcion drawDate()
+function drawDate(value)
+{
+      context.fillText(value,0,canvas.height);
+}
+
+
+//Funcion drawHands que hara las manecillas del reloj
+function drawHands()
+{
+      var date = new Date, 
+      hour = date.getHours();     
+      
+      //Operador ternario
+
+      //Condicion de pregunta ? Si es cierto es esto : Y si es falso es esto
+      hour = hour > 12 ? hour - 12 : hour; 
+      ////Puesto con if, pero lo podemos hacer con nuestro operador terciario
+      /*if(hour > 12)
+      {
+            hour = hour - 12
+      }
+      else 
+      hour = hour;*/
+      drawHand(hour * 5 + (date.getMinutes()/60)*5);
+      drawHand(date.getMinutes());
+      drawHand(date.getSeconds())
+      drawDate(date);
+      
+}
 //Inicializamos
 context.font = fontHeight + 'px Arial';
 loop = setInterval(drawClock, 1000); //El intervalo de tiempo
-
