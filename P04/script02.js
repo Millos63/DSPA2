3883075f53a55575c170407861c515de4395a048
Emigdio Espinosa <millosespinosa@gmail.com>
2022-09-19 11:56:07 -0500

    Terminamos P04 con los gradientes en distintos colores

--- P04/script02.js
+++ P04/script02.js
@@ -1,3 +1,16 @@
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
 
+
+var gradiente = ctx.createLinearGradient(0, 0, canvas.width, canvas.height);
+
+//Con los gradientes, se va degradando el color
+gradiente.addColorStop(0, "blue");
+gradiente.addColorStop(0.2, "orange");
+gradiente.addColorStop(0.5, "red");
+gradiente.addColorStop(0.8, "purple");
+gradiente.addColorStop(1, "yellow");
+
+ctx.fillStyle = gradiente;
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+
